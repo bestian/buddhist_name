d7cdb3920b70814c7774ef6ada3d4cbb41aa26a5
bestian <bestian@gmail.com>
2023-05-13 09:48:41 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,11 +7,12 @@
       </div>
     </div>
     <h1>加總</h1>
-    <table class="ui celled table">
+    <table class="ui celled unstackable table">
       <thead>
-        <tr><th><i class="user icon"/>姓名/法名</th>
+        <tr><th><i class="user icon"/><br class="thin-only" />姓名/法名</th>
         <!-- <th><i class="comment icon"/>佛號</th>  -->
-        <th><i class="plus icon"/>加總</th>
+        <!-- <th><i class="plus icon"/>加總</th> -->
+        <th><i class="align left icon"/>詳情</th>
       </tr></thead>
       <tbody>
         <tr v-for = "u in my_fil(users, key)" :key="u">
@@ -23,12 +24,28 @@
                </div>
              </div>
           </td> -->
-          <td data-label="Total">總共<br/>
-          <span class="highlight">{{ count(u) }}聲</span>
-          <br/>佛號</td>
+          <!-- <td data-label="Total">總共<br class="thin-only" />
+            <span class="highlight">{{ count(u) }}聲</span>
+            <br class="thin-only"/>佛號
+          </td> -->
+          <td>
+            <button class="ui big blue button" @click="expend(u)">
+              <i class="angle double down icon"/>
+              展開
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
+    <div class="ui left aligned list container" v-if="exp" id = "exp">
+      <div class="left aligned item">  
+        <h2 class="my_detail">{{exp}}：總共念了<span class="highlight">{{count(exp)}}聲</span>佛號</h2>
+      </div>
+      <div class="ui divider"></div>
+      <div class="left aligned item" v-for="(o, k) in my_exp(allnames, exp)" :key="k">
+        <p class="my_detail">{{o.date}}：念了<span class="highlight">{{o.number}}聲</span>佛號</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -39,8 +56,9 @@ export default {
   metaInfo: {
     title: '加總',
   },
-  props: ['names'],
+  props: ['names', 'allnames'],
   data: () => ({
+      exp: '',
       key: '',
       n: 5
   }),
@@ -49,9 +67,9 @@ export default {
     users () {
       var u = [];
       var key = this.key;
-      for (let i = 0; i < this.names.length; i++) {
+      for (let i = 0; i < this.allnames.length; i++) {
         var re = /(.+?)[/／|丨\s]/;
-        let n = this.names[i].n.replace(re, '');
+        let n = this.allnames[i].n.replace(re, '');
         re = /(.+)?[〈（](.+)[）〉].*/;
         n = n.replace(re, '$2');
         n = n.replace('丨','');
@@ -72,13 +90,42 @@ export default {
     const vm = this
     window.addEventListener("scroll", vm.handleScroll)
   },
+  watch: {
+    key () {
+      this.exp = ''
+    }
+  },
   methods: {
-    my_fil(list, key) {
+    expend (u) {
+      this.exp = u
+      setTimeout(() => {
+        const el = document.getElementById('exp')
+        el.scrollIntoView(true)
+      }, 100)
+    },
+    my_exp (list, key) {
+      if (!key) {
+        return []
+      }
+      var arr = [...list.filter((o) => {
+        // console.log(o)
+        let u = o.n || ''
+        var re = /(.+)?[/／|丨\s]+/;
+        u = u.replace(re, '');
+        re = /(.+)?[〈（](.+)[）〉].*/;
+        u = u.replace(re, '$2');
+        u = u.replace('丨','');
+        let pattern = new RegExp(key, 'i')
+        return pattern.test(u)
+      })]
+      arr.reverse()
+      return arr
+    },
+    my_fil (list, key) {
       if (!key) {
         return []
       }
       return list.filter((u) => {
-        console.log(u)
         var re = /(.+)?[/／|丨\s]+/;
         u = u.replace(re, '');
         re = /(.+)?[〈（](.+)[）〉].*/;
@@ -115,9 +162,9 @@ export default {
     },
     count (u) {
       var ans = 0;
-      for (var i = 0; i < this.names.length; i++) {
+      for (var i = 0; i < this.allnames.length; i++) {
         var re = /(.+)?[/／|丨\s]+/;
-        let n = this.names[i].n.replace(re, '');
+        let n = this.allnames[i].n.replace(re, '');
         u = u.replace(re, '');
         n = n.replace('丨','');
         re = /(.+)?[〈（](.+)[）〉].*/;
@@ -125,10 +172,9 @@ export default {
         u = u.replace('丨','');
         u = u.replace(re, '$2');
         if (u === n) {
-          ans += parseInt(this.names[i].number, 10);
+          ans += parseInt(this.allnames[i].number, 10);
         }
       }
-      console.log(ans)
       return ans;
     }
   }
@@ -141,9 +187,24 @@ td {
   max-width:33vw !important;
 }
 
+th {
+  text-align: center !important;
+}
+
+th i.icon {
+  margin: .3em !important;
+}
+
 @media screen and (max-width: 600px) {
   td {
     max-width:100vw !important;
+  }
+}
+
+@media screen and (min-width: 601px) {
+
+  .my_detail {
+    margin-left: 10vw;
   }
 }
 </style>
